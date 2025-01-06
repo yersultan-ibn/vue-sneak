c8ba7e7429dfa51c37b9631bc08b4b3dee360654
Yersultan <ye.sansyzbai@globerce.capital>
2025-01-06 13:42:55 +0500

    feat: add design layout archakov

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
-<script setup lang="ts">
-import MyHello from './components/MyHello.vue'
+<script setup>
+import HomePage from './pages/Home.vue';
+import Drawer from './components/Drawer.vue';
 </script>
 
-<style>
-body {
-  background: #fff;
-}
-h1,
-p,
-div {
-  color: #000;
-}
-</style>
-
 <template>
-  <div><h1>hello world</h1></div>
-  <MyHello title="Кроссы 1" :price="10000" :isAdded="true">
-    <button>Hello button</button>
-  </MyHello>
-  <MyHello title="Кроссы 2" :price="20000">
-    <h4>shak</h4>
-  </MyHello>
-  <MyHello title="Кроссы 3" :price="30000"></MyHello>
-  <MyHello title="Кроссы 4" :price="40000"></MyHello>
+  <!-- <Drawer /> -->
+  <HomePage />
 </template>
